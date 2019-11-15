30e7612b14cac5a0d148581d3d90c476f3220ee8
Paul Hobson <pmhobson@gmail.com>
2019-11-15 12:09:23 -0800

    ran black

--- application.py
+++ application.py
@@ -8,20 +8,20 @@ from ros import ROS
 app = Flask(__name__)
 
 
-@app.route('/echo', methods=['POST'])
+@app.route("/echo", methods=["POST"])
 def echo():
     return jsonify(request.json)
 
 
-@app.route('/as_arrays', methods=['POST'])
+@app.route("/as_arrays", methods=["POST"])
 def as_arrays():
     request.args
 
-    res = request.args.getlist('values', float)
-    cen = request.args.getlist('values', bool)
+    res = request.args.getlist("values", float)
+    cen = request.args.getlist("values", bool)
     imputed = ROS(res, cen)
     return jsonify(imputed)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
