4e54411fd16753e23910c826d13857717885e686
Paul Hobson <pmhobson@gmail.com>
2020-08-18 16:43:17 -0700

    get tests running

--- application.py
+++ application.py
@@ -1,5 +1,3 @@
-import json
-
 from flask import Flask
 from flask import request
 from flask import jsonify
@@ -12,14 +10,14 @@ from ros import ROS
 app = Flask(__name__)
 
 
-@app.route("/v1/echo", methods=["POST"])
+@app.route("/echo", methods=["GET"])
 def echo():
     return jsonify(request.json)
 
 
 @app.route("/v1/as_arrays", methods=["POST"])
 def as_arrays():
-    data = pandas.read_json(request.get_json()).assign(
+    data = pandas.DataFrame(request.get_json()).assign(
         censored=lambda df: df['censored'].astype(bool),
         values=lambda df: df['values'].astype(float)
     )
@@ -29,11 +27,21 @@ def as_arrays():
 
 @app.route("/v1/as_parts", methods=["POST"])
 def as_parts():
-    data = json.loads(request.get_json())
+    data = request.get_json()
     values = map(float, [*data['censored'], *data['uncensored']])
     censored = [*[True]*len(data['censored']), *[False]*len(data['uncensored'])]
     imputed = ROS(values, censored)
     return jsonify(imputed)
+
+
+@app.route('/v1/add', methods=['POST'])
+def add():
+    data = request.get_json()
+    return jsonify({
+        'sum': data['a'] + data['b'],
+        'a': data['a'],
+        'b': data['b']
+    })
 
 
 if __name__ == "__main__":
---
+++ test_app.py
@@ -0,0 +1,202 @@
+# from flask import json
+import json
+from flask import jsonify
+
+import pytest
+import numpy.testing as nptest
+
+
+from application import app
+
+DEV_URL = "http://127.0.0.1:5000"
+PROD_URL = "https://wqros.azurewebsites.net"
+
+as_array_data = {
+    "values": [
+        5.0,
+        5.0,
+        5.5,
+        5.75,
+        9.5,
+        9.5,
+        11.0,
+        2.0,
+        4.2,
+        4.62,
+        5.57,
+        5.66,
+        5.86,
+        6.65,
+        6.78,
+        6.79,
+        7.5,
+        7.5,
+        7.5,
+        8.63,
+        8.71,
+        8.99,
+        9.85,
+        10.82,
+        11.25,
+        11.25,
+        12.2,
+        14.92,
+        16.77,
+        17.81,
+        19.16,
+        19.19,
+        19.64,
+        20.18,
+        22.97,
+    ],
+    "censored": [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+    ],
+}
+
+as_parts_data = {
+    "censored": [5.0, 5.0, 5.5, 5.75, 9.5, 9.5, 11.0],
+    "uncensored": [
+        2.0,
+        4.2,
+        4.62,
+        5.57,
+        5.66,
+        5.86,
+        6.65,
+        6.78,
+        6.79,
+        7.5,
+        7.5,
+        7.5,
+        8.63,
+        8.71,
+        8.99,
+        9.85,
+        10.82,
+        11.25,
+        11.25,
+        12.2,
+        14.92,
+        16.77,
+        17.81,
+        19.16,
+        19.19,
+        19.64,
+        20.18,
+        22.97,
+    ],
+}
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        with app.app_context():
+            yield client
+
+
+@pytest.fixture
+def expected():
+    return [
+        3.11,
+        3.61,
+        4.05,
+        4.05,
+        4.71,
+        6.14,
+        6.98,
+        2.00,
+        4.20,
+        4.62,
+        5.57,
+        5.66,
+        5.86,
+        6.65,
+        6.78,
+        6.79,
+        7.50,
+        7.50,
+        7.50,
+        8.63,
+        8.71,
+        8.99,
+        9.85,
+        10.82,
+        11.25,
+        11.25,
+        12.20,
+        14.92,
+        16.77,
+        17.81,
+        19.16,
+        19.19,
+        19.64,
+        20.18,
+        22.97,
+    ]
+
+
+def test_add():        
+    response = app.test_client().post(
+        '/v1/add',
+        data=json.dumps({'a': 1, 'b': 2}),
+        content_type='application/json',
+    )
+
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data['sum'] == 3
+
+
+def test_echo(client):
+    response = client.get('/echo')
+    assert response.status_code == 200
+    assert response.content_type == 'application/json'
+
+
+@pytest.mark.parametrize(
+    ("endpoint", "data"), [
+        ("/v1/as_arrays", as_array_data),
+        ("/v1/as_parts", as_parts_data),
+    ]
+)
+def test_ros_endpoints(endpoint, data, expected, client):
+    response = client.post(endpoint, json=data)
+    assert response.status_code == 200
+    result = json.loads(response.get_data(as_text=True))
+    nptest.assert_almost_equal(result, expected, decimal=2)
